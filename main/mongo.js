bc7e597c57911476add4a36bff536624cd49a430
ananthajithraj <ananthajithraj@gmail.com>
2025-03-11 10:49:26 +0530

    Create mongo.js

---
+++ main/mongo.js
@@ -0,0 +1,42 @@
+
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length > 3) {
+  const person = new Person({
+    name: process.argv[2],
+    number: process.argv[3]
+  })
+
+  person.save().then((person) => {
+    console.log('added ' + person.name + ' ' + person.number + ' to phonebook')
+    mongoose.connection.close()
+  })
+
+} else {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+}
